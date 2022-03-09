8cffd2e153792043a60157fd829f938324759686
Alua <lauraortsramon@gmail.com>
2022-03-09 15:59:02 +0100

    Correccion de erroes

--- digimon.sql
+++ digimon.sql
@@ -17,8 +17,8 @@ CREATE TABLE Digimon(
     NomDigimon VARCHAR (20),
     Defensa INT (4) NOT NULL,
     Ataque INT (4) NOT NULL,
-    Tipo ENUM {'Vacuna','Virus','Animal','Planta','Elemental'} NOT NULL,
-    Nivel ENUM {'1','2','3'} NOT NULL,
+    Tipo ENUM ('Vacuna','Virus','Animal','Planta','Elemental') NOT NULL,
+    Nivel ENUM ('1','2','3') NOT NULL,
     NomEvoluviona VARCHAR (20),
     PRIMARY KEY (NomDigimon),
     CONSTRAINT fk_dig_dig FOREIGN KEY (NomEvoluviona) REFERENCES Digimon (NomDigimon) ON DELETE SET NULL ON UPDATE CASCADE
@@ -27,7 +27,7 @@ CREATE TABLE Digimon(
 CREATE TABLE Tiene(
     NombreUsu VARCHAR (20),
     NomDigimon VARCHAR (20),
-    Equipo ENUM {'Si','No'} NOT NULL,
+    Equipo ENUM ('Si','No') NOT NULL,
     PRIMARY KEY (NombreUsu, NomDigimon),
     CONSTRAINT fk_tiene_usuario FOREIGN KEY (NombreUsu) REFERENCES Usuario (NombreUsu) ON DELETE NO ACTION ON UPDATE CASCADE,
     CONSTRAINT fk_tiene_digimon FOREIGN KEY (NomDigimon) REFERENCES Digimon (NomDigimon) ON DELETE NO ACTION ON UPDATE CASCADE
