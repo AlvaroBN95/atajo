15ddc64ff809c86b2f106b1c3208e3272ca78de2
Alua <lauraortsramon@gmail.com>
2022-03-09 15:43:23 +0100

    Creacion sql

--- digimon.sql
+++ digimon.sql
@@ -1,19 +1,32 @@
+CREATE DATABASE Digimon
+CHARACTER SET latin1
+COLLATE latin1_spanish_ci;
 
+USE Digimon;
 
 CREATE TABLE Usuario(
-    NomUsu VARCHAR (10),
-    Pass   VARCHAR (9),
-    PartidasGan int (10),
-    Equipo ENUM {'S','N'},
-    PRIMARY KEY (NomUsu);)
+    NombreUsu VARCHAR (20),
+    Pass VARCHAR (20) NOT NULL,
+    PJugadas INT (6) NOT NULL,
+    PartidasGan INT (6) NOT NULL,
+    CantTokens INT (4) NOT NULL,
+    PRIMARY KEY (NomUsu));
 
-CREATE TABLE Digimons(
-    NomDig VARCHAR (20),
-    Tipo ENUM {''},
-    Nivel INT (1),
-    Ataque INT (3),
-    Defensa INT (3),
-    evolucion VARCHAR(20),
-    PRIMARY KEY (NomDig)
-    CONSTRAINT fk_dig_dig FOREIGN KEY (evolucion) REFERENCES Digimon (NomDig) ON DELETE CASCADE ON UPDATE CASCADE;)
+CREATE TABLE Digimon(
+    NomDigimon VARCHAR (20),
+    Defensa INT (4) NOT NULL,
+    Ataque INT (4) NOT NULL,
+    Tipo ENUM {'Vacuna','Virus','Animal','Planta','Elemental'} NOT NULL,
+    Nivel ENUM {'1','2','3'} NOT NULL,
+    NomEvoluviona VARCHAR (20),
+    PRIMARY KEY (NomDigimon),
+    CONSTRAINT fk_dig_dig FOREIGN KEY (NomEvoluviona) REFERENCES Digimon (NomDigimon) ON DELETE SET NULL ON UPDATE CASCADE);
 
+CREATE TABLE Tiene(
+    NombreUsu VARCHAR (20),
+    NomDigimon VARCHAR (20),
+    Equipo ENUM {'Si','No'} NOT NULL,
+    PRIMARY KEY (NombreUsu, NomDigimon),
+    CONSTRAINT fk_tiene_usuario FOREIGN KEY (NombreUsu) REFERENCES Usuario (NombreUsu) ON DELETE NO ACTION ON UPDATE CASCADE,
+    CONSTRAINT fk_tiene_digimon FOREIGN KEY (NomDigimon) REFERENCES Digimon (NomDigimon) ON DELETE NO ACTION ON UPDATE CASCADE
+);
