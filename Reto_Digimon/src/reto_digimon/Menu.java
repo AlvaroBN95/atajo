dbe12345d2ce91056a36a7d5e64ebb6de6461255
Alvaro <Alvaro@192.168.101.88>
2022-03-11 09:42:20 +0100

    Prueba clase menu

---
+++ Reto_Digimon/src/reto_digimon/Menu.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto_digimon;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class Menu {
+   public String AltaUsuario="";
+   public String Contraseña="";
+   
+   public Menu (String AltaU,String Contra){
+   
+   AltaUsuario=AltaU;
+   Contraseña=Contra;
+   
+   }
+}
